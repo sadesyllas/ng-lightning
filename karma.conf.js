df5f448f82b2f5d62ff4e59af535d17a73822fda
Tasos Bekos <tbekos@gmail.com>
2016-03-14 20:31:20 +0200

    refactor(build): move browser providers into separate file

--- karma.conf.js
+++ karma.conf.js
@@ -64,34 +64,7 @@ module.exports = function(config) {
   };
 
   if (isSaucelabs) {
-    cfg.customLaunchers =  {
-      'SL_Chrome': {
-        base: 'SauceLabs',
-        browserName: 'chrome'
-      },
-      'SL_FireFox': {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-      },
-      'SL_Safari_8': {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'OS X 10.10',
-        version: '8'
-      },
-      'SL_Safari_9': {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'OS X 10.11',
-        version: '9'
-      },
-      'SL_EDGE': {
-        base: 'SauceLabs',
-        browserName: 'microsoftedge',
-        platform: 'Windows 10',
-        version: '20.10240'
-      },
-    };
+    cfg.customLaunchers =  require('./test/browser-providers');
     cfg.browsers = Object.keys(cfg.customLaunchers);
     cfg.reporters.push('saucelabs');
     cfg.sauceLabs = {
---
+++ test/browser-providers.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'SL_Chrome': {
+    base: 'SauceLabs',
+    browserName: 'chrome'
+  },
+  'SL_FireFox': {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+  },
+  'SL_Safari_8': {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'OS X 10.10',
+    version: '8'
+  },
+  'SL_Safari_9': {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'OS X 10.11',
+    version: '9'
+  },
+  'SL_EDGE': {
+    base: 'SauceLabs',
+    browserName: 'microsoftedge',
+    platform: 'Windows 10',
+    version: '20.10240'
+  },
+};
