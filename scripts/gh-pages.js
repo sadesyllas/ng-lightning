2d2c0764fe72a68066d420854750298f8a565de3
Tasos Bekos <tbekos@gmail.com>
2016-08-08 13:21:16 +0300

    chore(scripts): always cleanup gh-pages before publishing

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -3,11 +3,12 @@ const path = require('path');
 const root = require('app-root-path').path;
 const pkg = require(`${root}/package.json`);
 
+ghpages.clean();
 ghpages.publish(path.join(__dirname, '../demo/dist'), {
   message: 'chore(release): v' + pkg.version,
 }, function (err) {
   if (err) {
-    console.log('Error while publishing demo.');
+    console.log('Error while publishing demo.', err);
     throw err;
   }
   console.log('Demo published!');
