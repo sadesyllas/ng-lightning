6173e5a735be0ae72f47d9e760b41f817c4b43cf
Tasos Bekos <tbekos@gmail.com>
2016-09-06 16:52:24 +0300

    chore(demo): add missing `FormsModule` in plunker
    This was causing `ngModel` not working inside paginations demo.
    
    Closes #196

--- demo/components/demo/playground/files/app/module.ts
+++ demo/components/demo/playground/files/app/module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NglModule, provideNglConfig } from 'ng-lightning/ng-lightning';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './demo';
 
 @NgModule({
-  imports:      [ BrowserModule, HttpModule, NglModule ],
+  imports:      [ BrowserModule, FormsModule, HttpModule, NglModule ],
   declarations: [ AppComponent ],
   providers:    [ provideNglConfig() ],
   bootstrap:    [ AppComponent ],
